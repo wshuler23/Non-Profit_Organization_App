99ee5467592397764509fe64fa5d38fa7c17f29b
dsuappacademy <wshuler23@gmail.com>
2016-07-13 16:28:53 -0400

    Initial commit

---
+++ app/src/main/java/edu/desu/dsuappacademy/non_profitorganizationapp/MainActivity.java
@@ -0,0 +1,36 @@
+package edu.desu.dsuappacademy.non_profitorganizationapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void call(View v) {
+        dialContactPhone("4103982470");
+    }
+
+    private void dialContactPhone(final String phoneNumber) {
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+    }
+
+
+    public void map(View view) {
+        Uri gmmIntentUri = Uri.parse("geo:39.6071231,-75.8396411");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null)
+
+        {
+            startActivity(mapIntent);
+        }
+    }
+}
