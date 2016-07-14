67b474fb9931198f21f8e9da61cc1653cf957f1d
dsuappacademy <wshuler23@gmail.com>
2016-07-14 18:19:02 -0400

    American Red Cross App [Updated]

--- app/src/main/java/edu/desu/dsuappacademy/non_profitorganizationapp/MainActivity.java
+++ app/src/main/java/edu/desu/dsuappacademy/non_profitorganizationapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView volunteer = (TextView) findViewById(R.id.volunteer);
+        volunteer.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent volunteerIntent = new Intent(MainActivity.this, Volunteer.class);
+
+                startActivity(volunteerIntent);
+
+            }
+        });
+
+        final TextView mission = (TextView) findViewById(R.id.mission);
+        mission.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent missionIntent = new Intent(MainActivity.this, Mission.class);
+
+                startActivity(missionIntent);
+
+            }
+        });
+
     }
 
     public void call(View v) {
@@ -21,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
     private void dialContactPhone(final String phoneNumber) {
         startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
     }
-
 
     public void map(View view) {
         Uri gmmIntentUri = Uri.parse("geo:39.6071231,-75.8396411");
